2386f10396f7c10264b3caced3dc6f1cbba01c99
Nigel Harsch <nigelharsch@gmail.com>
2016-10-28 01:05:37 -0400

    finished video9

--- github-battle/app/containers/ResultsContainer.js
+++ github-battle/app/containers/ResultsContainer.js
@@ -1,5 +1,6 @@
 var React = require('react');
 var Results = require('../components/Results');
+var githubHelpers = require('../utils/githubHelpers.js');
 
 var ResultsContainer = React.createClass({
     getInitialState: function () {
@@ -8,14 +9,22 @@ var ResultsContainer = React.createClass({
             scores: []
         }
     },
-    componentsDidMount: function () {
-        console.log(this.props);
+    componentDidMount: function () {
+        console.log(this.props.location.state.playersInfo);
+        githubHelpers.battle(this.props.location.state.playersInfo)
+            .then((scores) =>  {
+                this.setState({
+                    scores: scores,
+                    isLoading: false
+                })
+            }).bind(this)
     },
     render: function() {
         return (
            <Results
-           isLoading={this.state.isLoading}
-           restults={this.state.scores} />
+            isLoading={this.state.isLoading}
+            results={this.state.scores}
+            playersInfo={this.props.location.state.playersInfo }/>
         );
     }
 
--- github-battle/app/utils/githubHelpers.js
+++ github-battle/app/utils/githubHelpers.js
@@ -10,21 +10,36 @@ function getUserInfo (username) {
 
 function getRepos (username) {
   // fetch username repos
-  return axios.get(github_api_url + username + '/repos' + param + '')
+  return axios.get(github_api_url + username + '/repos' + param + '&per_page=100');
 }
 
-function getTotsalStarts (starts) {
+function getTotalStars (repos) {
   // calculate all the starts that the user has
+  return repos.data.reduce((prev, current) => {
+    return prev + current.stargazers_count
+  },0)
 }
 
 function getPlayersData (player) {
+  return getRepos(player.login)
+    .then(getTotalStars)
+    .then((totalStars) => {
+      return {
+        followers: player.followers,
+        totalStars: totalStars
+      }
+    })
 }
 
 function calculateScores (players) {
+  return [
+    players[0].followers * 3 + players[0].totalStars,
+    players[1].followers * 3 + players[1].totalStars,
+  ]
 }
 
 var helpers = {
-  getPlayersInfo: function (players) {
+  getPlayersInfo: (players) =>  {
     // axios takes array of promises
     return axios.all(players.map(function (username) {
       return getUserInfo(username)
@@ -36,8 +51,13 @@ var helpers = {
       console.warn('Error in getPlayersInfo', err);
     })
   },
-  battle: function (players) {
-    
+  battle: (players) =>  {
+    var playerOneData = getPlayersData(players[0]);
+    var playerTwoData = getPlayersData(players[1]);
+
+    return axios.all([playerOneData, playerTwoData])
+      .then(calculateScores)
+      .catch((err) => {console.warn('Error in getPlayersInfo: ', err)})
   }
 };
 
